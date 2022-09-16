1954290dcbc1eb1242e6c6f4566eb70523358559
STAN-65031 <lox.ibany@gmail.com>
2022-09-16 20:13:23 +0300

    did the job

---
+++ exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("введите текст");
+string text = Console.ReadLine();
+
+void FindLessThanThreeCharacters(string verifiable)
+{
+    char[] separators = new char[] { ' ', ',' };
+    string[] array = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[i] = array[i];
+        }
+
+    }
+
+    for (int i = 0; i < result.Length; i++)
+    {
+
+        Console.Write($"{result[i]} ");
+    }
+}
+
+FindLessThanThreeCharacters(text);
